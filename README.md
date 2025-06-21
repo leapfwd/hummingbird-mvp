a595076490b91458ca9bc9796cb9b16852a96c8b
leapfwd <jeremy@leapinnovations.us>
2025-06-20 21:08:26 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hummingbird MVP Starter
+
+Modern Next.js + Tailwind CSS starter for your agentless real estate platform.
---
+++ hello.js
@@ -0,0 +1,4 @@
+// Next.js API route example
+export default function handler(req, res) {
+  res.status(200).json({ name: 'Hummingbird API is working!' })
+}
